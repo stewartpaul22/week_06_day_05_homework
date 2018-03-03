a8b529cc5e22d42e2bc095f641c63dacd911983d
Paul Stewart <stewartpaul22@yahoo.co.uk>
2018-03-03 14:05:29 +0000

    Add getGuestCount(), checkInGuest() and checkOutGuest() to Room with tests

--- src/main/java/Room.java
+++ src/main/java/Room.java
@@ -4,4 +4,21 @@ public class Room {
 
     private ArrayList<Guest> guests;
 
+    public Room(ArrayList<Guest> guests) {
+        this.guests = guests;
+    }
+
+    public int getGuestCount() {
+        return this.guests.size();
+    }
+
+    public Guest checkInGuest(Guest guest) {
+        guests.add(guest);
+        return guest;
+    }
+
+    public Guest checkOutGuest(Guest guest) {
+        guests.remove(guest);
+        return guest;
+    }
 }
