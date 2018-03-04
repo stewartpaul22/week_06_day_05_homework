d1c40d78650de13d9b45d06950461b3e27b5626b
Paul Stewart <stewartpaul22@yahoo.co.uk>
2018-03-04 11:53:34 +0000

    Add Hotel class with initial properties and constructor

---
+++ src/main/java/Hotel.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+
+public class Hotel {
+
+    private ArrayList<Bedroom> bedrooms;
+    private ArrayList<Conference> conferenceRooms;
+    private ArrayList<Dining> diningRooms;
+
+    public Hotel(ArrayList<Bedroom> bedrooms, ArrayList<Conference> conferenceRooms, ArrayList<Dining> diningRooms) {
+        this.bedrooms = bedrooms;
+        this.conferenceRooms = conferenceRooms;
+        this.diningRooms = diningRooms;
+    }
+
+    // hotel can check guests in/out of rooms
+
+    // hotel can check which guests, if any, are checked into a particular room
+
+    // hotel can see a list of vacant bedrooms
+
+    // hotel can check in guests to bedrooms for a number of nights
+
+
+
+
+}
